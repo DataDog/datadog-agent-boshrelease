0c97124b67c6e3fb7fec4b6ab0791d742955d37e
Greg Meyer <gregory.meyer@datadoghq.com>
2018-03-13 17:13:07 -0400

    Flares (#31)
    * adds flare
    
    * flare works
    
    * better cleaner
    
    * removes commented out line
    
    * adds some comments
    
    * only prints out a happy message if curl succeeds
    
    * spelling

---
+++ jobs/datadog-firehose-nozzle/templates/clean_config.py
@@ -0,0 +1,34 @@
+import sys
+import json
+
+def clean_secret(secret, retain_last=False, truncate_length=False):
+    i = 0
+    cleaned_secret = ''
+    for c in secret:
+        if i > len(secret) - 6 and retain_last:
+            cleaned_secret += c
+        else:
+            cleaned_secret += '*'
+        if i == 5 and truncate_length:
+            return cleaned_secret
+        i += 1
+    return cleaned_secret
+
+def main():
+    if len(sys.argv) != 3:
+        print("please include the path to the config file as the first argument and the path to the cleaned file as the second")
+        sys.exit(1)
+    config_file_path=sys.argv[1]
+    cleaned_conig_file_path=sys.argv[2]
+    with open(config_file_path, 'r') as f:
+        config_file = f.read()
+
+    config_json = json.loads(config_file)
+    config_json['ClientSecret'] = clean_secret(config_json['ClientSecret'], truncate_length=True)
+    config_json['DataDogAPIKey'] = clean_secret(config_json['DataDogAPIKey'], retain_last=True)
+
+    with open(cleaned_conig_file_path, 'w') as f:
+        f.write(json.dumps(config_json, indent=4))
+
+if __name__ == '__main__':
+    main()
