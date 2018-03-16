ef257a738e4fbecc9318cfc4b6c5f39cb57464d6
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-16 14:19:18 -0400

    adds first version of release script

---
+++ scripts/release.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+IFS=$'\n\t'
+set -euxo pipefail
+
+# This script is to release the agent bosh release from Datadog's internal infrastructure.
+# It won't work for anyone else
+if [[ -z $VERSION ]]; then
+  echo "You must set a version"
+  exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORKING_DIR=$DIR/..
+
+mkdir -p $WORKING_DIR/blobstore
+
+# if bosh isn't on the docker image, download it
+if [ ! -f "/usr/local/bin/bosh" ]; then
+  mkdir -p $WORKING_DIR/bin
+  curl -sSL -o $WORKING_DIR/bin/bosh https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.48-linux-amd64
+  chmod +x $WORKING_DIR/bin/bosh
+  export PATH="$WORKING_DIR/bin:$PATH"
+fi
+
+# upload to the staging buckets if it's a staging release
+if [ "$STAGING" = "true" ]; then
+ cp $WORKING_DIR/config/final.yml.s3.staging $WORKING_DIR/config/final.yml
+fi
+
+# if it's production set the bucket to production
+if [ "$PRODUCTION" = "true" ]; then
+  cp $WORKING_DIR/config/final.yml.s3 $WORKING_DIR/config/final.yml
+fi
+
+# make sure we're in the right directory
+cd $WORKING_DIR
+
+# run the prepare script
+./prepare
+bosh sync-blobs
+echo '{}' > config/private.yml
+# release a dev version of the agent to ensure the cache is warm
+# (it's better to fail here than to fail when really attempting to release it)
+bosh create-release --force --name "datadog-agent"
+
+# if it's a try run, then set the bucket to a local bucket
+# we have to make sure the cache is warm first
+if [ "$DRY_RUN" = "true" ]; then
+  cp $WORKING_DIR/config/final.yml.s3.local $WORKING_DIR/config/final.yml
+fi
+
+# finally, release the agent
+./release
+# make sure we upload the blobs
+bosh upload-blobs
+
+# git commit it and then push it to the repo
+git add .
+git commit -m "releases datadog agent $VERSION"
+git push
+
+# cache the blobs
+cp blobstore/* archive/blobstore
+cp datadog-agent-release.tgz archive/datadog-agent-release.tgz
+
+if [ "$RELEASE_BUCKET" ]; then
+  aws s3 cp datadog-agent-release.tgz s3://$RELEASE_BUCKET/datadog-agent-boshrelease-$VERSION.tgz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=3a6e02b08553fd157ae3fb918945dd1eaae5a1aa818940381ef07a430cf25732
+
+  aws s3 cp datadog-agent-release.tgz s3://$RELEASE_BUCKET/datadog-agent-boshrelease-latest.tgz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=3a6e02b08553fd157ae3fb918945dd1eaae5a1aa818940381ef07a430cf25732
+fi
