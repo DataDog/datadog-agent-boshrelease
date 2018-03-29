e7bc36d92620bc1639e72c576bb66d18abe19345
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-29 16:39:18 -0400

    only if production

--- scripts/release.sh
+++ scripts/release.sh
@@ -100,8 +100,10 @@ cp -R $WORKING_DIR/blobstore archive/blobstore
 cp $WORKING_DIR/datadog-agent-release.tgz archive/datadog-agent-release.tgz
 
 if [ "$RELEASE_BUCKET" -a "$RELEASE_BUCKET" != "false" ]; then
-  # the production release bucket is cloudfoundry.datadoghq.com/datadog-agent
-  aws s3 cp datadog-agent-release.tgz s3://$RELEASE_BUCKET/datadog-agent-boshrelease-$VERSION.tgz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=3a6e02b08553fd157ae3fb918945dd1eaae5a1aa818940381ef07a430cf25732
+  if [ "$PRODUCTION" = "true" ]; then
+    # the production release bucket is cloudfoundry.datadoghq.com/datadog-agent
+    aws s3 cp datadog-agent-release.tgz s3://$RELEASE_BUCKET/datadog-agent-boshrelease-$VERSION.tgz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=3a6e02b08553fd157ae3fb918945dd1eaae5a1aa818940381ef07a430cf25732
 
-  aws s3 cp datadog-agent-release.tgz s3://$RELEASE_BUCKET/datadog-agent-boshrelease-latest.tgz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=3a6e02b08553fd157ae3fb918945dd1eaae5a1aa818940381ef07a430cf25732
+    aws s3 cp datadog-agent-release.tgz s3://$RELEASE_BUCKET/datadog-agent-boshrelease-latest.tgz --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=3a6e02b08553fd157ae3fb918945dd1eaae5a1aa818940381ef07a430cf25732
+  fi
 fi
