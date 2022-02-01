f46ecbcce8f5d45a9d886e565c097578c02ca7ba
NouemanKHAL <noueman.khal@gmail.com>
2022-02-02 00:17:56 +0100

    handle case where dd-agent already exists

--- src/helpers/agent-setup.sh
+++ src/helpers/agent-setup.sh
@@ -17,8 +17,10 @@ function ensure_agent_ownership {
   chown -R vcap:vcap "$JOB_DIR/packages/dd-agent/"
   # make sure that root owns the system probe config
   chown root:root "$JOB_DIR/config/system-probe.yaml" && chmod 0644 "$JOB_DIR/config/system-probe.yaml"
-  # add dd-agent user as an alias of vcap
-  sudo useradd -N -r -s /usr/sbin/nologin -u $(id -u vcap) -g vcap -o dd-agent
+  # make sure that dd-agent user exists as an alias of vcap
+  if ! id dd-agent &>/dev/null; then
+    sudo useradd -N -r -s /usr/sbin/nologin -u $(id -u vcap) -g vcap -o dd-agent
+  fi
 }
 
 
