e7c313f5c9bf61d2058091bb0983fc345893c380
Sarah <sarah.witt@datadoghq.com>
2022-12-05 15:04:36 -0500

    Fix parsing

--- src/helpers/parse_python_path.py
+++ src/helpers/parse_python_path.py
@@ -2,9 +2,7 @@ import sys
 
 if len(sys.argv) > 1:
     env_file_name = sys.argv[1]
-    with open(env_file_name, 'w+') as env_file: 
+    with open(env_file_name, 'w+') as env_file:
         for python_path in sys.path:
             if python_path:
                 env_file.write("PYTHON_PATH={}:$PYTHONPATH\n".format(python_path))
-
-        
