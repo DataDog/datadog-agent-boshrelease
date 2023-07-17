edae34e1f85cc5b1e623b64ed922febf14f33dca
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2023-07-17 16:39:55 +0200

    remove local blobstore usage during releease

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,13 +9,5 @@ if [ ! -f ./config/private.yml ]; then
   echo "you need to set up your private.yml"
 fi
 
-# setting up the local blobstore
-cp config/final.yml config/final.yml.bk
-cp config/final.yml.local config/final.yml
-
 # Creating the release
 bosh create-release --force --final --tarball=datadog-agent-release.tgz --name datadog-agent --version "$VERSION"
-
-# restore the previous config
-cp config/final.yml.bk config/final.yml
-rm config/final.yml.bk
